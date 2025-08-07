34398a4d115d53610ae441ee2183cf1abc150f87
Yash <your_email@example.com>
2025-08-07 10:43:47 +0530

    changed port number

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -20,6 +20,6 @@ if [ "$(docker ps -aq -f name=hybrid_recsys)" ]; then
 fi
 
 echo "Starting new container..."
-docker run -d -p 8000:8000 --name hybrid_recsys 561903439839.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+docker run -d -p 80:8000 --name hybrid_recsys 561903439839.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
 
 echo "Container started successfully."
